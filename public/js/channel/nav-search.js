f6fe9f4431bedceee65a0e27d0276a6777562eae
yoonhyunjin02 <hyeonjin-hyunjin@naver.com>
2025-05-08 16:30:16 +0900

    feat: 채널 페이지 네비 검색 예외처리 추가

--- public/js/channel/nav-search.js
+++ public/js/channel/nav-search.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const searchIcon = document.getElementById('searchIcon');
     const searchInput = document.getElementById('searchInput');
+    const searchForm = searchInput.closest('form'); // form 태그 가져오기
 
     searchIcon.addEventListener('click', () => {
         searchInput.classList.toggle('active');
@@ -8,6 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
             searchInput.focus();
         } else {
             searchInput.value = '';
+        }
+    });
+
+    searchForm.addEventListener('submit', (e) => {
+        const query = searchInput.value.trim();
+        if (query === '') {
+            e.preventDefault(); // form 제출 막기
+            alert('검색어를 입력하세요.');
+            window.location.href = `/channel/${channelId}/Home`;
         }
     });
 });
