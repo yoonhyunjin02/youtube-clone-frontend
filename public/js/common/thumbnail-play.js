ab4722c8079b4a3fce5843c660d1e7b9d1c3b6bc
sungyeonkim <awers1010@gmail.com>
2025-05-08 17:44:46 +0900

    fix 비디오 썸네일 재생 시 에러 메세지 생성 제거

--- public/js/common/thumbnail-play.js
+++ public/js/common/thumbnail-play.js
@@ -2,16 +2,19 @@ document.querySelectorAll(".video-card").forEach(card => {
     const video = card.querySelector("video");
 
     card.addEventListener("mouseenter", () => {
-        if (video) {
-        video.currentTime = 0;
-        video.play();
+        if (video && video.paused) {
+            video.currentTime = 0;
+            video.play().catch((e) => {
+                //플레이 요청이 실패할 경우 에러를 무시
+                console.debug("Video play interrupted:", e);
+            });
         }
     });
 
     card.addEventListener("mouseleave", () => {
-        if (video) {
-        video.pause();
-        video.currentTime = 0;
+        if (video && !video.paused) {
+            video.pause();
+            video.currentTime = 0;
         }
     });
 });
