e908826af2a1acbff4daaa35f55500562e206799
jwLee <jwljwljwl777@gmail.com>
2025-05-10 00:46:41 +0900

    feat: appear-modal 추가가

--- public/js/common/buttonHeader.js
+++ public/js/common/buttonHeader.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const noticeImg = noticeBtn.querySelector('img');
     const profileBtn=document.querySelector('.profile-btn');
     const profileModal = document.querySelector('.profile-modal');
+    const appearanceBtn= document.querySelector('.appearance-btn');
+    const appearanceModal = document.querySelector('.appear-modal');
+    const backBtn= document.querySelector('.back-btn');
     creatBtn.addEventListener('click', (e) => {
         e.stopPropagation();
         profileModal.style.display = 'none';
@@ -44,6 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('click', (e) => {
         if (noticeModal.style.display === 'flex') {
             noticeModal.style.display = 'none';
+            noticeImg.src='/assets/icons/notifications.svg';
         }
     });
 
@@ -66,4 +70,37 @@ document.addEventListener('DOMContentLoaded', function() {
             profileModal.style.display = 'none';
         }
     });
+
+    appearanceBtn.addEventListener('click', (e) => {
+        e.stopPropagation();
+        profileModal.style.display = 'none';
+        appearanceModal.style.display = 'flex';
+    });
+
+    document.addEventListener('click', (e) => {
+        if (appearanceModal.style.display === 'flex') {
+            appearanceModal.style.display = 'none';
+        }
+    });
+
+    backBtn.addEventListener('click', (e) => {
+        e.stopPropagation(); 
+        appearanceModal.style.display = 'none';
+        profileModal.style.display = 'flex';
+    });
+
+    const themeButtons = document.querySelectorAll('.appear-modal-main button');
+    const darkThemeBtn = document.querySelector('[data-theme="dark"]');
+    const lightThemeBtn = document.querySelector('[data-theme="light"]');
+
+    themeButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            themeButtons.forEach(btn => btn.classList.remove('active'));
+            e.currentTarget.classList.add('active');
+            const selectedTheme = e.currentTarget.dataset.theme;
+            document.body.classList.toggle('light-theme', selectedTheme === 'light');
+        });
+    });
+    
+    darkThemeBtn.classList.add('active');
 });
