89ef5e0c0f84f9d9fde4ddfb64a06d09603bb416
sungyeonkim <awers1010@gmail.com>
2025-05-11 18:26:30 +0900

    fix 테마 토글 오류 수정 및 기능 수정

--- public/js/common/theme-toggle.js
+++ public/js/common/theme-toggle.js
@@ -1,18 +1,42 @@
 //theme-toggle.js
 const youtubeLogoImg = document.querySelector('.youtube-logo');
 const themeButtons = document.querySelectorAll('[data-theme]');
+const appearanceBtn = document.querySelector('.appearance-btn');
 
 themeButtons.forEach(button => {
   button.addEventListener('click', () => {
     const selectedTheme = button.dataset.theme; 
 
-    document.body.classList.remove('dark-mode', 'light-mode');
-    document.body.classList.add(`${selectedTheme}-mode`);
+    document.body.classList.remove('dark-theme', 'light-theme');
+    document.body.classList.add(`${selectedTheme}-theme`);
 
-    if (document.body.classList.contains('light-mode')) {
+    // 브라우저에 테마 저장
+    localStorage.setItem('theme', selectedTheme);
+
+    if (document.body.classList.contains('light-theme')) {
       youtubeLogoImg.src = '/assets/toggleicons/youtube-logo.svg';
+      appearanceBtn.innerHTML = '<img src="/assets/icons/appearance-sun.svg" alt="Appearance: Device theme"><span>Appearance: light theme</span>';
     } else {
       youtubeLogoImg.src = '/assets/logos/Youtube-Logo.svg';
+      appearanceBtn.innerHTML = '<img src="/assets/icons/appearance.svg" alt="Appearance: Device theme"><span>Appearance: dark theme</span>';
     }
+
   });
+});
+
+//페이지 로드 시 테마 적용 코드
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    document.body.classList.remove('light-theme', 'dark-theme');
+    document.body.classList.add(`${savedTheme}-theme`);
+  }
+
+  if (document.body.classList.contains('light-theme')) {
+    youtubeLogoImg.src = '/assets/toggleicons/youtube-logo.svg';
+    appearanceBtn.innerHTML = '<img src="/assets/icons/appearance-sun.svg" alt="Appearance: Device theme"><span>Appearance: light theme</span>';
+  } else {
+    youtubeLogoImg.src = '/assets/logos/Youtube-Logo.svg';
+    appearanceBtn.innerHTML = '<img src="/assets/icons/appearance.svg" alt="Appearance: Device theme"><span>Appearance: dark theme</span>';
+  }
 });
